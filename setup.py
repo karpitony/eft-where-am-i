7b98031b9076c5f8f6b793e2ce2f6157123aa9b3
Yunseok <watemark1017@gmail.com>
2024-05-25 22:52:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ description = "Description of EFT Where am I"
 
 # 필요한 DLL 파일 경로 추가
 dll_files = [
-    ("C:/path/to/MSVCP140.dll", "MSVCP140.dll"),  # Visual C++ 재배포 가능 패키지 DLL
-    ("C:/path/to/VCRUNTIME140.dll", "VCRUNTIME140.dll")  # 추가 DLL 파일
+    ("C:/Windows/SysWOW64/MSVCP140.dll", "MSVCP140.dll"),  # Visual C++ 재배포 가능 패키지 DLL
+    ("C:/Windows/SysWOW64/VCRUNTIME140.dll", "VCRUNTIME140.dll")  # 추가 DLL 파일
 ]
 
 # 패키지와 파일 포함 옵션 설정
