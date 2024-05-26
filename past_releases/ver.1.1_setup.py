1e007adf807c1ec49fe8c0ea8b18adf9098bc39a
Yunseok <watemark1017@gmail.com>
2024-05-26 22:22:17 +0900

    ver.1.1 archive

---
+++ past_releases/ver.1.1_setup.py
@@ -0,0 +1,39 @@
+from cx_Freeze import setup, Executable
+import os
+import sys
+
+name = "EFT Where am I"
+version = "1.2"
+description = "Description of EFT Where am I"
+
+# 필요한 DLL 파일 경로 추가
+dll_files = [
+    ("C:/path/to/MSVCP140.dll", "MSVCP140.dll"),  # Visual C++ 재배포 가능 패키지 DLL
+    ("C:/path/to/VCRUNTIME140.dll", "VCRUNTIME140.dll")  # 추가 DLL 파일
+]
+
+# 패키지와 파일 포함 옵션 설정
+options = {
+    'build_exe': {
+        'packages': ["tkinter", "webbrowser", "keyboard", "os", "glob", "time", "selenium"],
+        'includes': ["tkinter.ttk"],
+        'include_files': dll_files,
+        'excludes': [],
+    }
+}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [
+    Executable("main.py", base=base, target_name="EFT_Where_am_I.exe")
+]
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    options=options,
+    executables=executables
+)
