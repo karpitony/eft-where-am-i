914f332e9731e25d7cbc434fa8ee4628457ffec1
Yunseok <watemark1017@gmail.com>
2024-05-27 15:17:37 +0900

    add translation

---
+++ translation/app_en.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="main.py" line="208"/>
+        <source>Select The Map.</source>
+        <translation type="Select The Map."></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="222"/>
+        <source>Apply</source>
+        <translation type="Apply"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="229"/>
+        <source>Auto Screenshot Detection</source>
+        <translation type="Auto Screenshot Detection"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="244"/>
+        <source>Hide/Show Pannels</source>
+        <translation type="Hide/Show Pannels"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="249"/>
+        <source>Full Screen</source>
+        <translation type="Full Screen"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="254"/>
+        <source>Force Run</source>
+        <translation type="Force Run"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="275"/>
+        <source>Apply Language</source>
+        <translation type="Apply Language"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="282"/>
+        <source>How to use</source>
+        <translation type="How to use"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="288"/>
+        <source>Bug Report</source>
+        <translation type="Bug Report"></translation>
+    </message>
+</context>
+</TS>
---
+++ translation/app_ko.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>BrowserWindow</name>
+    <message>
+        <location filename="main.py" line="208"/>
+        <source>Select The Map.</source>
+        <translation type="맵을 선택해주세요."></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="222"/>
+        <source>Apply</source>
+        <translation type="적용"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="229"/>
+        <source>Auto Screenshot Detection</source>
+        <translation type="스크린샷 파일 자동 감지"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="244"/>
+        <source>Hide/Show Pannels</source>
+        <translation type="패널 숨기기/보이기"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="249"/>
+        <source>Full Screen</source>
+        <translation type="전체 화면"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="254"/>
+        <source>Force Run</source>
+        <translation type="강제 실행"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="275"/>
+        <source>Apply Language</source>
+        <translation type="언어 적용"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="282"/>
+        <source>How to use</source>
+        <translation type="사용 방법"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="288"/>
+        <source>Bug Report</source>
+        <translation type="버그 제보"></translation>
+    </message>
+</context>
+</TS>
