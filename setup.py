d71e9600a098b1bafaddce2e98bd8d0907324ec2
Yunseok <watemark1017@gmail.com>
2024-05-28 13:47:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,47 @@
 from cx_Freeze import setup, Executable
+import os
 import sys
 
+name = "EFT Where am I"
+version = "1.2"
+description = "A Python program to easily get location in Tarkov"
+
+# 필요한 DLL 파일 경로 추가
+dll_files = [
+    ("C:/Windows/SysWOW64/MSVCP140.dll", "MSVCP140.dll"),  # Visual C++ 재배포 가능 패키지 DLL
+    ("C:/Windows/SysWOW64/VCRUNTIME140.dll", "VCRUNTIME140.dll")  # 추가 DLL 파일
+]
+
+include_files = dll_files + [
+    ('translations/', 'translations/'),  # 번역 파일 폴더 포함
+    ('settings.json', 'settings.json')  # settings.json 파일 포함
+]
+
 # 빌드 옵션 설정
-build_exe_options = {
-    "packages": ["os", "glob", "time", "sys"],
-    "includes": ["PyQt5", "PyQt5.QtCore", "PyQt5.QtGui", "PyQt5.QtWidgets", "PyQt5.QtWebEngineWidgets", "PyQt5.QtWebEngineCore"],
-    "include_files": [
-        ('translations/', 'translations/'),  # 번역 파일 폴더 포함
-        ('settings.json', 'settings.json')  # settings.json 파일 포함
-    ],
-    "excludes": ["tkinter", "unittest"],
-    "zip_include_packages": ["encodings", "PyQt5"],
+options = {
+    "build_exe": {
+        "packages": ["os", "glob", "time", "sys"],
+        "includes": ["PyQt5", "PyQt5.QtCore", "PyQt5.QtGui", "PyQt5.QtWidgets", "PyQt5.QtWebEngineWidgets", "PyQt5.QtWebEngineCore"],
+        "include_files": include_files,
+        "excludes": [],
+        'include_msvcr': True,
+        'silent': True
+    }
 }
 
 # Windows에서 GUI 애플리케이션을 빌드할 때 base 설정
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+    
+executables = [
+    Executable("main.py", base=base, target_name="EFT_Where_am_I.exe")
+]
 
-# setup 함수 호출
 setup(
-    name="EFT Where am I",
-    version="1.2",
-    description="A Python program to easily get locations in Tarkov",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("main.py", base=base, target_name="EFT_Where_am_I.exe")]
+    name=name,
+    version=version,
+    description=description,
+    options=options,
+    executables=executables
 )
